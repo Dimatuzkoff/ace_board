0e10f37b364b41e28aa98b30aa326340d2ae1da1
Dimatuzkoff <dimatuzkoff@gmail.com>
2025-02-03 22:28:41 +0200

    added fonnt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
 export default defineNuxtConfig({
     devtools: { enabled: false },
-    modules: ['@nuxt/image', '@nuxtjs/tailwindcss', 'shadcn-nuxt', '@nuxtjs/color-mode'],
+    modules: ['@nuxt/image', '@nuxtjs/tailwindcss', 'shadcn-nuxt', '@nuxtjs/color-mode',
+        ['@nuxtjs/google-fonts', {
+            families: {
+                Lato: {
+                    wght: [300, 400, 700],
+                    ital: [300],
+                },
+            }
+        }],],
     shadcn: {
         prefix: 'Ui',
         componentDir: './components/ui'
