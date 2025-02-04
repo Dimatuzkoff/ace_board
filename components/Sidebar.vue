5d80e8da788e960b28731e811ba507e9fe0b27c2
Dimatuzkoff <dimatuzkoff@gmail.com>
2025-02-04 21:05:41 +0200

    added nuxt-icon

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -7,8 +7,8 @@
       <NuxtImg src="/logo.svg" alt="logo" width="100px" class="mx-auto" />
     </NuxtLink>
     <!-- <UiButton>Send</UiButton> -->
-    <button
-      class="absolute top-2 right-3 transition-colors hover:text-purple-400"
-    ></button>
+    <button class="absolute top-2 right-3 transition-colors hover:text-primary">
+      <Icon name="line-md:logout" size="20" />
+    </button>
   </aside>
 </template>
---
+++ components/layout/menu.data.ts
@@ -0,0 +1,16 @@
+export interface IMenuItem {
+    name: string;
+    url: string;
+    icon: string;
+}
+
+export const MENU_DATA: IMenuItem[] = [{
+    icon: "radix-icons:dashboard",
+    name: 'Home',
+    url: '/'
+}, {
+    name: 'Products',
+    url: '/products',
+    icon: 'ep:goods'
+},
+]
