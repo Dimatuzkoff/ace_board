762f459d8bf161494a8bc3d1e25415ffb8194779
Dimatuzkoff <dimatuzkoff@gmail.com>
2025-02-06 15:03:28 +0200

    created login form

---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <div class="p-10">
+    <h1 class="font-bold text-2x1 mb-10">CRM System "ACE BOARD"</h1>
+  </div>
+</template>
---
+++ pages/login.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: "Login | CRM System",
+});
+
+const emailRef = ref("");
+const passwordRef = ref("");
+const nameRef = ref("");
+
+// watch(emailRef, () => {
+//   console.log(emailRef.value);
+// });
+</script>
+<template>
+  <div class="flex items-center justify-center min-h-screen w-full">
+    <div class="rounded bg-sidebar w-1/4 p-5">
+      <h1 class="text-2xl font-bold text-center mb-5">Login</h1>
+      <form>
+        <UiInput
+          placeholder="Email"
+          type="email"
+          class="bm-3"
+          v-model="emailRef"
+        />
+        <UiInput
+          placeholder="Password"
+          type="password"
+          class="bm-3"
+          v-model="passwordRef"
+        />
+        <UiInput
+          placeholder="Name"
+          type="name"
+          class="bm-3"
+          v-model="nameRef"
+        />
+        <div class="flex items-center justify-center gap-5">
+          <UiButton>Login</UiButton>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
