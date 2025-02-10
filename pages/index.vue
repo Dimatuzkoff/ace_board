cfd5bb017770faf0f3294cdbc63eb4ef412efdb8
Dimatuzkoff <dimatuzkoff@gmail.com>
2025-02-10 13:28:55 +0200

    created card

--- pages/index.vue
+++ pages/index.vue
@@ -4,5 +4,12 @@
 <template>
   <div class="p-10">
     <h1 class="font-bold text-2x1 mb-10">CRM System "ACE BOARD"</h1>
+    <div class="">
+      <UiCard class="mb-3" draggable="true">
+        <UiCardHeader role="button">name card</UiCardHeader>
+        <UiCardContent> Компания </UiCardContent>
+        <UiCardFooter> Date </UiCardFooter>
+      </UiCard>
+    </div>
   </div>
 </template>
