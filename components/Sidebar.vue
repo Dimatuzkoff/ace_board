9f30c273351fc9a0e875bd3544fcbbce3800e4b2
Dimatuzkoff <dimatuzkoff@gmail.com>
2025-02-10 09:56:09 +0200

    added logout

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,4 +1,17 @@
 <script lang="ts" setup>
+import { useIsLoadingStore, useAuthStore } from "~/store/auth.store";
+
+const store = useAuthStore();
+const isLoadingStore = useIsLoadingStore();
+const router = useRouter();
+
+async function logout() {
+  isLoadingStore.set(true);
+  await account.deleteSession("current");
+  store.clear();
+  await router.push("/login");
+  isLoadingStore.set(false);
+}
 </script>
 
 <template>
@@ -7,7 +20,10 @@
       <NuxtImg src="/logo.svg" alt="logo" width="120px" class="mx-auto" />
     </NuxtLink>
     <!-- <UiButton>Send</UiButton> -->
-    <button class="absolute top-2 right-3 transition-colors hover:text-primary">
+    <button
+      class="absolute top-2 right-3 transition-colors hover:text-primary"
+      @click="logout"
+    >
       <Icon name="line-md:logout" size="20" />
     </button>
     <LayoutMenu />
