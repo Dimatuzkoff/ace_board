548e1e9de64634881aa31d9557dde667b1da598e
Dimatuzkoff <dimatuzkoff@gmail.com>
2025-03-03 20:11:36 +0200

    creaded id customer page

--- pages/customers/edit/[id].vue
+++ pages/customers/edit/[id].vue
@@ -1,3 +1,11 @@
+<script lang="ts" setup>
+import { storage } from "~/utils/appwrite";
+import { useMutation, useQuery } from "@tanstack/vue-query";
+import { v4 as uuid } from "uuid";
+import { COLLECTION_CUSTOMERS, DB_ID, STORAGE_ID } from "~/app.constants";
+import type { ICustomer } from "~/types/deals.types";
+</script>
+
 <template>
   <div></div>
 </template>
