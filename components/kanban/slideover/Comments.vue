d7657e3d561f82b2d76bf0d5c43157db3b544c46
Dimatuzkoff <dimatuzkoff@gmail.com>
2025-03-03 12:07:12 +0200

    added comments.vue

--- components/kanban/slideover/Comments.vue
+++ components/kanban/slideover/Comments.vue
@@ -1,8 +1,20 @@
 <script setup lang="ts">
 import { useDealSlideStore } from "~/store/deal-slide.store";
+import dayjs from "dayjs";
+import type { IDeal } from "~/types/deals.types";
+import { useComments } from "./useComments";
+import { useCreateComment } from "./useCreateComment";
 
 const store = useDealSlideStore();
+const { data, refetch, isLoading } = useComments();
+const { commentRef, writeComment } = useCreateComment({ refetch });
+
+const card = data as unknown as IDeal;
 </script>
 <template>
-  <div class=""></div>
+  <UiInput
+    placeholder="Оставьте комментарий"
+    v-model="commentRef"
+    @keyup.enter="writeComment"
+  />
 </template>
